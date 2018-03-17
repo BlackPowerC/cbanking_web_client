84e44ccb72150d2751c44bf3de621291ae003032
fatigba <fatigba72@gmail.com>
2018-03-17 16:58:00 +0000

    feature(page de connexion): connexion au serveur. Depuis la page de connexion on peut se connecter au serveur REST.

--- application/controllers/Signin.php
+++ application/controllers/Signin.php
@@ -9,7 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 defined('VENDOR') OR exit('No direct script access allowed');
 
 require_once VENDOR."autoload.php" ;
-use GuzzleHttp\Client ;
+
+use Guzzle\Http\Client ;
+use Guzzle\Http\Exception\RequestException ;
 
 /**
  * @Class Signin
@@ -21,7 +23,7 @@ class Signin extends CI_Controller
     {
         parent::__construct() ;
         // autoloading
-        $this->load->library('form_validation') ;
+        $this->load->library(['session', 'form_validation']) ;
         $this->load->helper(['url', 'form']) ;
     }
 
@@ -38,11 +40,38 @@ class Signin extends CI_Controller
 
         if($this->form_validation->run())
         {
-
+            if($this->session->has_userdata('token') and $this->session->userdata('token') !== "")
+            {
+                // redirection vers la page personnelle
+                exit() ;
+            }
+            else
+            {
+                /* envoie des données au serveur REST */
+                try
+                {
+                    $ids = json_encode(['email'=>$this->input->post('e-mail'),
+                        "passwd"=>$this->input->post('passwd')]);
+                    var_dump($ids) ;
+                    $client = new Client() ;
+                    $request = $client->post("http://localhost:8181/authentification", [], $ids);
+                    $response = $request->send() ;
+                    echo '<div class="alert alert-info">';
+                    echo "request body from my REST API ".$response->getBody()."</br>" ;
+                    echo '</div>';
+                    echo '<div class="alert alert-info">';
+                    echo "request status from my REST API ".$response->getStatusCode()."</br>" ;
+                    echo '</div>';
+                }catch(RequestException $exception)
+                {
+                    echo $exception->getMessage().'</br>';
+                    ?>
+                    <a href="signin">Réessayer</a>
+                    <?php
+                    die(-1) ;
+                }
+            }
         }
-        else
-        {
-            $this->load->view("signin") ;
-        }
+        $this->load->view("signin") ;
     }
 }
