85cd52b4bff40717c089c5dea6c9fde0549810a3
fatigba <fatigba72@gmail.com>
2018-03-18 22:43:25 +0000

    feature(page d'inscription): validation de formulaire. J'ai implémenter dans le controlleur Signup la validation du formulaire ainsi que les fonction check_*. Il ne me reste qu'à écrire leur test.

--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -27,28 +27,73 @@ class Signup extends CI_Controller
     }
 
     /**
-     * Cette fonction affiche le formulaire de connexion.
+     * Cette fonction affiche le formulaire de inscription.
      */
     public function index()
     {
+        /* validation de formulaire */
+        // Le nom
+        $this->form_validation->set_rules("name", "nom réel",
+            ['required', 'strip_tags', 'trim', 'callback_check_name'],
+            ['required'=>'Le {field} est obligatoire',
+             'check_name'=>'Le {field} ne peut contenir que des caractères alphabétiques']) ;
+        // L'email
+        $this->form_validation->set_rules("e-mail", "addresse électronique",
+            ['required', 'strip_tags', 'trim'],
+            ['required'=>'L\' {field} est obligatoire']) ;
+        // Le mot de passe
+        $this->form_validation->set_rules("passwd", "mot de passe",
+            ['required', 'strip_tags', 'trim', 'callback_check_passwd'],
+            ['required'=>'L\' {field} est obligatoire',
+                'check_passwd'=>'Le {field} doit contenir au moins 6 caractères avec des chiffres et des lettres']) ;
+
+        if($this->form_validation->run())
+        {
+            $ids = [
+               'name'=>$this->input->post("name"),
+               'e-mail'=>$this->input->post("e-mail"),
+               'passwd'=>$this->input->post("passwd"),
+               'type'=>$this->input->post("type")
+            ] ;
+            var_dump(json_encode($ids)) ;
+        }
         $this->load->view("signup") ;
     }
 
     /**
      * Cette fonction vérifie si un nom contient des caractères illégaux.
      * @param string $name
+     * @return bool
      */
-    public function check_name(string $name)
+    public function check_name(string $name): bool
     {
-
+        return preg_match("/^[a-zA-Z]$/", $name) ;
     }
 
     /**
      * Cette fonction vérifie la robustesse des mot de passe.
      * @param string $password
+     * @return bool.²
      */
-    public function check_password(string $password)
+    public function check_password(string $password): bool
     {
-
+        $num = 0;
+        $char = 0;
+        if(strlen($password) < 6)
+        {
+            return FALSE;
+        }
+        foreach ($password as $char)
+        {
+            if(is_int($char))
+            {
+                $num += 1 ;
+            }
+            else
+            {
+                $char += 1 ;
+            }
+        }
+        return ($num > 0 && $char > 0) ;
     }
 }
