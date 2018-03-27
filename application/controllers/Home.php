9cde2b8d852cbfe6ac180a5bb1a99d1e650be4cf
fatigba <fatigba72@gmail.com>
2018-03-27 15:16:54 +0000

    feature(home page): ajout de la methode getDataFromAPI. J'ai ajouté la méthode getDataFromAPI pour faciliter les requêtes vers l'API REST. Cela simplifie grandement le corps de la méthode index.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -14,51 +14,64 @@ require_once VENDOR."autoload.php" ;
 use Guzzle\Http\Client ;
 use Guzzle\Http\Exception\RequestException ;
 
+/**
+ * Class Home.
+ * Cette classe est le controlleur de la page personnelle de
+ * l'employé.
+ */
 class Home extends CI_Controller
 {
+    /**
+     * Ce tableau contient les données à afficher dans la vue.
+     * @array
+     */
+    private $data ;
     public function __construct()
     {
         parent::__construct() ;
-        $this->load->library(['session', 'form_validation']) ;
-        $this->load->helper(['url', 'form']) ;
     }
 
-    public function index()
+    /**
+     * Cette fonction récupère depuis cbankin_rest_api à une URL, des données.
+     * @param array $params Un tableau de paramettre à deux clé, url et token.
+     * @return array|bool|float|int|null|string
+     */
+    public function getDataFromAPI(array $params = ['url'=>'localhost:8181', 'token'=>'23321425024549421161211921618911418213413092729622315176208194194234106118104173101211616736141415951461566075101219662762140166180182112771166623369121591147657224'])
     {
-        $data['subordinates'] = null ;
         $request = null ;
         $response = null ;
-        // Récupération des infos depuis l'API Rest
         try
         {
             $client = new Client() ;
-            $token['token'] = $this->session->userdata('token') ;
-            $request = $client->post("http://localhost:8181/employee/subordinate/get/all", [], json_encode($token)) ;
+            $request = $client->post($params['url'], [], json_encode(['token'=>$params['token']])) ;
             $response = $request->send() ;
-            $data['subordinates'] = $response->getBody(TRUE) ;
-            var_dump($_SESSION) ;
+            return $response->json() ;
         }
         catch(RequestException $re)
         {
             echo $re->getMessage().'<br/>' ;
             echo $request->getBody().'<br/>' ;
-            $data['subordinates'] = json_decode('[
-	{"id":1, "name":"titianne", "email":"titianne@gmail.com", "passwd":""},
-{"id":3, "name":"thierrno", "email":"thierrno@gmail.com", "passwd":""},
-{"id":15, "name":"koffi", "email":"tintin@gmail.com", "passwd":""},
-{
-"id":16,
-"name":"ablavi",
-"email":"abla@gmail.com",
-"passwd":"",
-"accounts":
-	[
-	{"id":1, "creationDate":"26-02-2018", "balance":1998.950000}
-	]
-},
-{"id":17, "name":"dalida", "email":"dalida@gmail.com", "passwd":""},
-{"id":18, "name":"nabine", "email":"nabin@gmail.com", "passwd":""}	]', TRUE) ;
         }
-        $this->load->view("home", $data) ;
+        return null ;
+    }
+
+    public function index()
+    {
+
+        $this->load->library(['session', 'form_validation']) ;
+        $this->load->helper(['url', 'form']) ;
+
+        $this->data['customers'] = $this->getDataFromAPI([
+            'url'=>'http://localhost:8181/customer/get/all',
+            'token'=>$this->session->userdata('token')
+        ]) ;
+
+        $this->data['subordinates'] = $this->getDataFromAPI([
+            'url'=>'http://localhost:8181/employee/subordinate/get/all',
+            'token'=>$this->session->userdata('token')
+        ]) ;
+
+        // Récupération des infos depuis l'API Rest
+        $this->load->view("home", $this->data) ;
     }
 }
