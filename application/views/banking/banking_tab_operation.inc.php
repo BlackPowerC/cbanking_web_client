ce31d81835dbc91bf14310408a5b0099f4820ae4
fatigba <fatigba72@gmail.com>
2018-04-02 12:01:46 +0000

    feature(transaction banquaire): depot et retrait.
    Je viens d'implémenter la fonctionnalité de retait et de dépot sur
    le client web. Cela est assuré par le controlleur Banking et sa
    fonction operation. Il me reste à ajouter des feed-back.

--- application/views/banking/banking_tab_operation.inc.php
+++ application/views/banking/banking_tab_operation.inc.php
@@ -10,11 +10,45 @@
     <div class="card">
         <div class="card-body">
             <?php
-            echo validation_errors('<div class="alert alert-warning">', '</div>') ;
-            echo form_open("");
-            //
-            echo form_input() ;
+            echo form_open("banking/operation");
             ?>
+                <!-- Type de transaction -->
+                <div class="form-group">
+                    <select name="type" title="Type d'opération" class="form-control">
+                        <option value="0">Dépot</option>
+                        <option value="1">Retrait</option>
+                    </select>
+                </div
+                <!-- Numero de compte -->
+                <div class="form-group">
+                <select title="Numero de compte" class="form-control" name="id_account">
+                    <?php
+                    if(array_key_exists('accounts', $customer))
+                    {
+                        foreach($customer['accounts'] as $account)
+                        {?>
+                            <option value="<?php echo $account['id'] ;?>"><?php echo $account['id'] ;?></option>
+                        <?php
+                        }
+                    }
+                    ?>
+                </select>
+                </div>
+                <!-- Montant de la transaction -->
+                <div class="form-group">
+                    <?php
+                    echo form_input([
+                            "name"=>"balance",
+                            "type"=>"number",
+                            "placeholder"=>"Montant",
+                            "class"=>"form-control",
+                            "title"=>"Montant de la transaction"
+                    ]) ;
+                    // Champ caché pour l'id du client
+                    echo form_hidden("id_customer", $customer['id']);
+                ?>
+                </div>
+                <input style="width: 100%" id="btn-submit" class="btn btn-primary" type="submit" value="Envoyer" />
             </form>
         </div>
     </div>
