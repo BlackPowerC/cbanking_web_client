a67fb643a1600110ddc7424b7504f59453c71dc9
fatigba <fatigba72@gmail.com>
2018-04-04 12:14:51 +0000

    Ajout de getCustomerByName.js.
    Ce fichier doit contenir une requête AJAX, pour avoir une liste de client
    selon leur nom.

---
+++ js/getCustomerByName.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jordy on 04/04/18.
+ */
+
+/* Requête AJAX pour récupérer la client ayant un nom
+ * commencant par un mot clé.
+ **/
+
+var request = new XMLHttpRequest() ;
+request.onreadystatechange = function () {
+
+    if(request.readyState === 4) {
+
+        if(request.status === 200) {
+
+            /* modification du DOM avec affichage de la liste */
+        }
+
+        else {
+
+            /* modification du DOM avec affichage de not found */
+        }
+    }
+}
