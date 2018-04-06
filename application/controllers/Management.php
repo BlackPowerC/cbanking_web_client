fa0763a801de7eb84b5cbf6b564d1fe46b0f8aa6
fatigba <fatigba72@gmail.com>
2018-04-06 12:53:48 +0000

    Ajout du controlleur Management.
    Ce controlleur sera utilisé pour les pages en liens avec la gestion
    des clients et des subordonnés.

---
+++ application/controllers/Management.php
@@ -0,0 +1,49 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed') ;
+/**
+ * Created by PhpStorm.
+ * User: jordy
+ * Date: 06/04/18
+ * Time: 11:27
+ */
+class Management extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct() ;
+    }
+
+    /**
+     * Affiche la page de listing des subordonnés.
+     */
+    public function subordinates()
+    {
+
+    }
+
+    /**
+     * Affiche la page de profil d'un employé.
+     * @param $id L'identifiant de l'employé.
+     */
+    public function subordinate($id)
+    {
+
+    }
+
+    /**
+     * Affiche la page de listing de tout les clients
+     */
+    public function customers()
+    {
+
+    }
+
+    /**
+     * Affiche la page d'inscription d'un employé ou d'un client.
+     */
+    public function subscription()
+    {
+
+    }
+}
