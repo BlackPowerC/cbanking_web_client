fa4517149beafdf0ef7d9e8dbf2369535a5fe311
fatigba <fatigba72@gmail.com>
2018-04-25 09:59:55 +0000

    Découpage du pîed de page des pages.

--- application/views/ace/banking/account.php
+++ application/views/ace/banking/account.php
@@ -91,31 +91,7 @@
             </div>
         </div><!-- /.main-content -->
 
-        <div class="footer">
-            <div class="footer-inner">
-                <div class="footer-content">
-						<span class="bigger-120">
-							<span class="blue bolder">CBANKING WEB CLIENT</span>
-							Application &copy; 2018
-						</span>
-
-                    &nbsp; &nbsp;
-                    <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
-						</span>
-                </div>
-            </div>
-        </div>
+        <?php $this->load->view("ace/footer.inc.php") ?>
 
         <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
             <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
--- application/views/ace/management/persons.php
+++ application/views/ace/management/persons.php
@@ -54,7 +54,7 @@
 
                                     <div class="space-20"></div>
 
-                                    <?php $this->load->view($view) ?>  
+                                    <?php $this->load->view($view) ?>
 
                                     <div class="hr hr2 hr-double"></div>
 
@@ -70,31 +70,7 @@
             </div>
         </div><!-- /.main-content -->
 
-        <div class="footer">
-            <div class="footer-inner">
-                <div class="footer-content">
-						<span class="bigger-120">
-							<span class="blue bolder">Ace</span>
-							Application &copy; 2013-2014
-						</span>
-
-                    &nbsp; &nbsp;
-                    <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
-						</span>
-                </div>
-            </div>
-        </div>
+        <?php $this->load->view("ace/footer.inc.php") ?>
 
         <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
             <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
